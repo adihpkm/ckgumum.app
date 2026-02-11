9e22e557e7218f1f5661268476dff70f234ab2e8
adihpkm <adihpkm@gmail.com>
2026-02-11 09:06:33 +0700

    Update pasang_ckg.user.js

--- pasang_ckg.user.js
+++ pasang_ckg.user.js
@@ -4,14 +4,16 @@
 // @description  Instalasi Otomatis Daftar & Skrining CKG
 // @author       Adih
 // @match        https://sehatindonesiaku.kemkes.go.id/*
+// @match        https://form.kemkes.go.id/*
+// @run-at       document-idle
 // @require      https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js
 // @require      https://raw.githubusercontent.com/adihpkm/ckgumum.app/refs/heads/main/daftar_ckgumum.js
 // @require      https://raw.githubusercontent.com/adihpkm/ckgumum.app/refs/heads/main/skrining_ckgumum.js
 // @grant        GM_setValue
 // @grant        GM_getValue
 // @grant        GM_deleteValue
-// @updateURL    https://raw.githubusercontent.com/adihpkm/ckgumum.app/refs/heads/main/pasang_ckg.user.js
-// @downloadURL  https://raw.githubusercontent.com/adihpkm/ckgumum.app/refs/heads/main/pasang_ckg.user.js
+// @updateURL    https://raw.githubusercontent.com/adihpkm/ckgumum.app/refs/heads/main/app_ckgumum.user.js
+// @downloadURL  https://raw.githubusercontent.com/adihpkm/ckgumum.app/refs/heads/main/app_ckgumum.user.js
 // ==/UserScript==
 
 (function() {
