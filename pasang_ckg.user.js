ff706772168ae6bf38d63e6c2638831c122c089b
adihpkm <adihpkm@gmail.com>
2026-02-11 08:38:02 +0700

    app_ckgumum.user.js

---
+++ pasang_ckg.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         CKG Umum Puskesmas Kosambi
+// @version      1.0
+// @description  Instalasi Otomatis Daftar & Skrining CKG
+// @author       Adih
+// @match        https://sehatindonesiaku.kemkes.go.id/*
+// @require      https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js
+// @require      https://raw.githubusercontent.com/adihpkm/ckgumum.app/refs/heads/main/daftar_ckgumum.js
+// @require      https://raw.githubusercontent.com/adihpkm/ckgumum.app/refs/heads/main/skrining_ckgumum.js
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_deleteValue
+// @updateURL    https://raw.githubusercontent.com/adihpkm/ckgumum.app/refs/heads/main/pasang_ckg.user.js
+// @downloadURL  https://raw.githubusercontent.com/adihpkm/ckgumum.app/refs/heads/main/pasang_ckg.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log("Sistem CKG Aktif!");
+})();
